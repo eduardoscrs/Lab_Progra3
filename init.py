87933407955b63bdff04674fe2c818d18933c47e
eduardo <eduardoscrs60@gmail.com>
2025-04-18 21:23:10 -0400

    Update init.py

--- init.py
+++ init.py
@@ -1,10 +1,10 @@
-from db import init_db
+# from db import init_db
 
-if __name__ == "__main__":
-    init_db()
-    print("✅ Base de datos inicializada correctamente.")
-    print("✅ Tablas creadas correctamente.")
+# if __name__ == "__main__":
+#     init_db()
+#     print("✅ Base de datos inicializada correctamente.")
+#     print("✅ Tablas creadas correctamente.")
 
-#Init para que no de error al importar el módulo
-#? ejecutar con el comando python init.py
-#en la terminal
+# #Init para que no de error al importar el módulo
+# #? ejecutar con el comando python init.py
+# #en la terminal
